85283ec93abe67ea2a93e1f08d45688ea54c66bc
merillpr <merillpr16@gmail.com>
2024-03-11 14:25:48 +0700

    [+] header

--- App.js
+++ App.js
@@ -3,16 +3,52 @@ import FetchScreen from "./FetchScreen";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Image } from "react-native";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Flexbox">
-        <Stack.Screen name="Flexbox" component={FlexboxScreen} />
-        <Stack.Screen name="Fetch" component={FetchScreen} />
+      <Stack.Navigator
+        initialRouteName="Flexbox"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#ffa500" },
+          headerTintColor: "black",
+          headerTitleStyle: {
+            fontWeight: "300",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Flexbox"
+          component={FlexboxScreen}
+          options={{
+            title: "Flexbox Option", // terganti
+            headerTitle: () => {
+              return <FlexboxLogoTitle />;
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Fetch"
+          component={FetchScreen}
+          options={{
+            title: "Fetch Data",
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const FlexboxLogoTitle = () => {
+  return (
+    <Image
+      style={{ width: 40, height: 40 }}
+      source={{
+        uri: "https://w7.pngwing.com/pngs/452/495/png-transparent-react-javascript-angularjs-ionic-github-text-logo-symmetry-thumbnail.png",
+      }}
+    />
+  );
+};
